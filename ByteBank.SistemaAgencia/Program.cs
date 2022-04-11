cfcdc1e7d8fe9f16e2cd240d9e9391c269cea322
Alberto Da Silva Araujo <alberto.araujo@cea.com.br>
2022-04-11 15:22:15 -0300

    implemantando regex com expressões regulares.

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBank.Modelos;
 using Humanizer;
 using System;
+using System.Text.RegularExpressions;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -8,13 +9,31 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-           
+            Console.ReadLine();
+        }
+
+        static void TestaString()
+        {
+            // "[123456789][123456789][123456789][123456789][-][123456789][123456789][123456789][123456789]"
+            // "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            // "[0-9]{4,5}[-][0-9]{4}";
+            // "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            // "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoDeTeste = "Meu nome é Alberto, me ligue em 99404-0805";
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine(resultado.Value);
+
+            Console.ReadLine();
 
             string urlTeste = "https://www.bytebank.com/cambio";
             int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
 
             Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
             Console.WriteLine(urlTeste.EndsWith("cambio"));
+
+            Console.ReadLine();
 
             string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
 
